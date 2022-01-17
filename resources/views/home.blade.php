322a7c20c4cca70aee7857c43a1fb11b07482cf2
Lsantac <Lsantac@gmail.com>
2022-01-17 16:08:07 -0300

    mapas de ofertas e necessidades na tela inicial

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -130,9 +130,214 @@
                   </div>
             </div>
           </div>
+
+          <br>
+
+          
+          <div class="row">
+            <div class="col">
+              <div class="card" >
+                <div style="height:30rem;" class="card-body" id="map_of">
+                  
+                </div>
+              </div>
+            </div>
+            <div class="col">
+              <div class="card" >
+                <div style="height:30rem;" class="card-body" id="map_nec">
+                  
+                </div>
+              </div>
+            </div>
+          </div>
+
+        
       
     </div>
-    
 
+    <script>
+      var customLabel = {
+      restaurant: {
+          label: 'R'
+      },
+      bar: {
+          label: 'B'
+      },
+      
+      };
+
+    function initMap_of() {
+      var lati = <?php echo Session::get('latitude'); ?>;   
+      var longi = <?php echo Session::get('longitude'); ?>;   
+      
+      var map = new google.maps.Map(document.getElementById('map_of'), {
+          center: new google.maps.LatLng(lati, longi),
+          zoom: 5
+      });
+      
+      var infoWindow = new google.maps.InfoWindow;
+      
+      // Change this depending on the name of your PHP or XML file
+      downloadUrl('/mapas/resultado_of.php', function(data) {
+      var xml = data.responseXML;
+      var markers = xml.documentElement.getElementsByTagName('marker');
+      Array.prototype.forEach.call(markers, function(markerElem) {
+          var color = markerElem.getAttribute('color'); 
+          var name = markerElem.getAttribute('name');
+          var address = markerElem.getAttribute('address');
+          var type = markerElem.getAttribute('type');
+      
+          //pega posição pelo endereço
+          var geocoder = new google.maps.Geocoder();
+          
+          geocoder.geocode( { address: address}, function(results, status) {
+      
+          if (status == google.maps.GeocoderStatus.OK) {
+              var latitude = results[0].geometry.location.lat();
+              var longitude = results[0].geometry.location.lng();
+              //var latlng = results[0].geometry.location.LatLng();
+              //alert('Latitude : ' + latitude);
+             
+              } 
+          }); 
+          //===================================================================================
+      
+          var point = new google.maps.LatLng(
+              parseFloat(markerElem.getAttribute('lat')),
+              parseFloat(markerElem.getAttribute('lng')));
+      
+          //var point = '('+latitude+','+longitude+')';
+          //  alert(point);
+      
+          var infowincontent = document.createElement('div');
+          var strong = document.createElement('strong');
+          strong.textContent = name
+          infowincontent.appendChild(strong);
+          infowincontent.appendChild(document.createElement('br'));
+      
+          var text = document.createElement('text');
+          text.textContent = address
+          infowincontent.appendChild(text);
+          var icon = customLabel[type] || {};
+      
+          var marker = new google.maps.Marker({
+              map: map,
+              position: point,
+              
+              label: icon.label,
+              icon: {url: color}
+          });
+      
+          marker.addListener('click', function() {
+                  infoWindow.setContent(infowincontent);
+                  infoWindow.open(map, marker);
+                  
+          });
+          
+      });
+      });
+      }
+
+      function initMap_nec() {
+      var lati = <?php echo Session::get('latitude'); ?>;   
+      var longi = <?php echo Session::get('longitude'); ?>;   
+      
+      var map = new google.maps.Map(document.getElementById('map_nec'), {
+          center: new google.maps.LatLng(lati, longi),
+          zoom: 5
+      });
+      
+      var infoWindow = new google.maps.InfoWindow;
+      
+      // Change this depending on the name of your PHP or XML file
+      downloadUrl('/mapas/resultado_nec.php', function(data) {
+      var xml = data.responseXML;
+      var markers = xml.documentElement.getElementsByTagName('marker');
+      Array.prototype.forEach.call(markers, function(markerElem) {
+          var color = markerElem.getAttribute('color'); 
+          var name = markerElem.getAttribute('name');
+          var address = markerElem.getAttribute('address');
+          var type = markerElem.getAttribute('type');
+      
+          //pega posição pelo endereço
+          var geocoder = new google.maps.Geocoder();
+          
+          geocoder.geocode( { address: address}, function(results, status) {
+      
+          if (status == google.maps.GeocoderStatus.OK) {
+              var latitude = results[0].geometry.location.lat();
+              var longitude = results[0].geometry.location.lng();
+              //var latlng = results[0].geometry.location.LatLng();
+              //alert('Latitude : ' + latitude);
+             
+              } 
+          }); 
+          //===================================================================================
+      
+          var point = new google.maps.LatLng(
+              parseFloat(markerElem.getAttribute('lat')),
+              parseFloat(markerElem.getAttribute('lng')));
+      
+          //var point = '('+latitude+','+longitude+')';
+          //  alert(point);
+      
+          var infowincontent = document.createElement('div');
+          var strong = document.createElement('strong');
+          strong.textContent = name
+          infowincontent.appendChild(strong);
+          infowincontent.appendChild(document.createElement('br'));
+      
+          var text = document.createElement('text');
+          text.textContent = address
+          infowincontent.appendChild(text);
+          var icon = customLabel[type] || {};
+      
+          var marker = new google.maps.Marker({
+              map: map,
+              position: point,
+              
+              label: icon.label,
+              icon: {url: color}
+          });
+      
+          marker.addListener('click', function() {
+                  infoWindow.setContent(infowincontent);
+                  infoWindow.open(map, marker);
+                  
+          });
+          
+      });
+      });
+      }
+      
+      function downloadUrl(url, callback) {
+      var request = window.ActiveXObject ?
+      new ActiveXObject('Microsoft.XMLHTTP') :
+      new XMLHttpRequest;
+      
+      request.onreadystatechange = function() {
+      if (request.readyState == 4) {
+      request.onreadystatechange = doNothing;
+      callback(request, request.status);
+      }
+      };
+      
+      request.open('GET', url, true);
+      request.send(null);
+      }
+      
+      function doNothing() {}
+      </script>
+      
+      
+      <script    
+      src="https://maps.googleapis.com/maps/api/js?key=AIzaSyBQZKTit2ice6KDwHxAc5iQVZQhoBwimjw&callback=initMap_of&libraries=&v=weekly"
+      async
+      ></script>
+
+      <script    
+      src="https://maps.googleapis.com/maps/api/js?key=AIzaSyBQZKTit2ice6KDwHxAc5iQVZQhoBwimjw&callback=initMap_nec&libraries=&v=weekly"
+      async
+      ></script>
 
 @endsection
