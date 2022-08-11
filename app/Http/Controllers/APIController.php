61326744ee7972be09463071d15c36c2f87e3e6a
Lsantac <Lsantac@gmail.com>
2022-08-11 20:17:57 -0300

    ajustes api

--- app/Http/Controllers/APIController.php
+++ app/Http/Controllers/APIController.php
@@ -12,11 +12,12 @@ class APIController extends Controller
     function consulta_part(Request $request)
     {
 
-        $part = db::table('participantes')->where('id',$request->id_part)->first();
+        $part = db::table('participantes')->all();
 
+        $id = $part->id;
         $nome = $part->nome_part;
         $email = $part->email;
         
-        return response()->json(['nome' => $nome, 'email' => $email]);
+        return response()->json(['nome' => $nome, 'email' => $email, 'id' => $id]);
     }
 }
--- routes/api.php
+++ routes/api.php
@@ -20,4 +20,4 @@ Route::middleware('auth:api')->get('/user', function (Request $request) {
 });
 
 
-Route::get('consulta_part',  [APIController::class, 'consulta_part']);
+Route::get('/consulta_part/{id_part}',  [APIController::class, 'consulta_part']);
